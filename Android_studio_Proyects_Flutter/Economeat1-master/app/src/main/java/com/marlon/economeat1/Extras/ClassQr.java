b15512997e2e5a43242136d316dc7a51f2b99167
Marlon Francois <marlonrorodriguez2b@gail.com>
2020-03-20 20:05:20 -0600

    version 6

---
+++ Android_studio_Proyects_Flutter/Economeat1-master/app/src/main/java/com/marlon/economeat1/Extras/ClassQr.java
@@ -0,0 +1,14 @@
+package com.marlon.economeat1.Extras;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClassQr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_qr);
+    }
+}
